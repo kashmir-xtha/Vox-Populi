ffa904e9fd10eba1fefaa9355ac6b829b712a7d1
Knox1837 <kaustuvm1837@gmail.com>
2026-02-13 23:12:13 +0545

    Vox-Populi V2.5

---
+++ README.md
@@ -0,0 +1,71 @@
+# Vox-Populi: Online voting system
+**Hosted at:** []()
+<img width="1863" height="867" alt="Screenshot 2026-02-13 211510" src="https://github.com/user-attachments/assets/89c76a5e-b27c-4ab3-b005-eaf6ad4a364f" />
+
+The Online Voting System is a secure and efficient digital platform designed to facilitate fair and transparent elections within an organization or college community. Voters can cast their ballots electronically, view candidate information, and see real-time results while administrators can manage positions, approve candidates, and monitor election activities.
+
+## Key Features:
+1. Secure Authentication: Role-based login system for voters, candidates, and administrators
+2. Candidate Applications: Users can apply for candidacy with photo and statements
+3. Admin Approval System: Administrators review and approve/reject candidate applications
+4. Voter Dashboard: Clean interface for viewing candidates by position and casting votes
+5. One Voter, One Vote: Prevents double voting through database constraints
+6. Real-time Results: Live vote counting and result visualization
+7. Position Management: Admins can create and manage election positions
+8. Responsive Design: Seamless experience across desktop, tablet, and mobile devices
+
+## Tech Stack:
+* Framework: Flask (Python)
+* Database: PostgreSQL
+* Authentication: bcrypt for password hashing
+* API: RESTful endpoints with JSON responses
+* Frontend: React/Vite
+* Styling: Tailwind CSS
+* Icons: Lucide React
+
+## System Requirements:
+- **Python**: v3.8 or above
+- **PostgreSQL**: v12 or above
+- **pip**: Python package manager
+- **Git**: For cloning the repository
+- **Node.js**: v18 or above
+
+## Installation & Setup Guide
+**Clone the repository**
+```bash
+git clone [https://github.com/kashmir-xtha/Vox-Populi]
+cd Vox-Populi
+```
+
+### To run the backend system locally:
+
+1. **Install dependencies**
+```bash
+pip install -r requirements.txt
+```
+   
+2. **Initialize database**
+```bash
+py backend/init_db.py
+```
+3. **Run backend**
+```bash
+py backend/app.py
+```
+
+### To run the frontend system locally:
+1. **Install dependencies**
+```bash
+npm install
+```
+2. **Run the local frontend server**
+```bash
+cd frontend
+npm run dev
+```
+3. **Access the Application**
+```bash
+http://localhost:5173/
+```
+
+
--- frontend/src/components/VoterBallot.jsx
+++ frontend/src/components/VoterBallot.jsx
@@ -1,11 +1,42 @@
 import { useNavigate } from "react-router-dom"
-import { useEffect } from "react"
-import image from "../assets/homepage_image.png"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export default function VoterBallot() {
     const navigate = useNavigate()
+    const [candidates, setCandidates] = useState([])
+    const [positions, setPositions] = useState([])
+
+    const fetchPositions = async () => {
+            try {
+                const response = await axios.get("http://localhost:5000/api/positions")
+                if (response.data.positions) {
+                    setPositions(response.data.positions)
+                }
+            }
+            catch (error) {
+                console.log(error)
+            }
+        }
+        
+        const fetchCandidates = async () => {
+            try {
+                const response = await axios.get("http://localhost:5000/api/candidates")
+                console.log(response.data.candidates)
+                if (response.data.candidates) {
+                    setCandidates(response.data.candidates.filter(
+                        (candidate) => candidate.status === "approved"
+                    ))
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
 
     useEffect(() => {
+        fetchPositions()
+        fetchCandidates()
+
         const token = localStorage.getItem("token")
         const role = token ? JSON.parse(atob(token)).role : ""
 
@@ -19,78 +50,60 @@ export default function VoterBallot() {
                 <h1 className="text-3xl font-black leading-tight tracking-[-0.033em] mb-3">Voter Election Ballot</h1>
                 <p className="text-[#617589] text-base max-w-xl mx-auto">Select one candidate for each of the following positions. After making your selections, review your ballot for final submission.</p>
             </div>
-            <section className="mb-12">
-                <div className="flex items-center justify-between mb-4 px-2">
-                    <h2 className="text-xl font-bold flex items-center gap-2">
-                        <span className="size-2 bg-[#137fec] rounded-full"></span>
-                        President
-                    </h2>
-                    <span
-                        className="text-xs font-bold text-[#137fec] uppercase tracking-widest bg-[#137fec]/10 px-2 py-1 rounded">Choose
-                        1</span>
-                </div>
-                <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-                    <div
-                        class="bg-white rounded-xl border border-[#f0f2f4] overflow-hidden group hover:shadow-xl hover:shadow-[#137fec]/5 transition-all duration-300">
-                        <div class="relative h-48 overflow-hidden">
-                            <div class="h-full w-full bg-cover bg-center transition-transform duration-500 group-hover:scale-105"
-                                data-alt="Candidate Elena Rodriguez portrait">
-                                    <img src={image} alt="" />
-                                </div>
+            <section>
+                {/* repeat this for each position  */}
+                {positions.map((position, index) => {
+
+                    // get candidates for this position
+                    const filteredCandidates = candidates.filter(
+                        item => item.position === position[1]
+                    );
+
+                    // 🚫 if none exist, render nothing
+                    if (filteredCandidates.length === 0) return null;
+
+                    return (
+                        <div key={index} className="mb-12">
+                            <div className="flex items-center justify-between mb-4 px-2">
+                                <h2 className="text-xl font-bold flex items-center gap-2">
+                                    <span className="size-2 bg-[#137fec] rounded-full"></span>
+                                    {position[1]}
+                                </h2>
+
+                                <span className="text-xs font-bold text-[#137fec] uppercase tracking-widest bg-[#137fec]/10 px-2 py-1 rounded">
+                                    Choose 1
+                                </span>
+                            </div>
+
+                            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+                                {filteredCandidates.map((item, index) => (
+                                    <div key={index}
+                                        className="bg-white rounded-xl border border-[#f0f2f4] overflow-hidden group hover:shadow-xl hover:shadow-[#137fec]/5 transition-all duration-300">
+
+                                        <div className="relative h-48 overflow-hidden">
+                                            <img src={item.photo_url} alt="Candidate Photo" />
+                                        </div>
+
+                                        <div className="p-5">
+                                            <h3 className="text-xl font-bold mb-1">
+                                                {item.full_name}
+                                            </h3>
+
+                                            <p className="text-sm h-32 text-gray-500 line-clamp-6 mb-4 leading-relaxed">
+                                                {item.statement}
+                                            </p>
+
+                                            <button className="cursor-pointer w-full flex items-center justify-center gap-2 rounded-lg h-10 bg-[#137fec] text-white text-sm font-bold">
+                                                Cast Vote
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                        <div class="p-5">
-                            <h3 class="text-xl font-bold mb-1">test</h3>
-                            <p
-                                class="text-sm text-gray-500 line-clamp-6 mb-4 leading-relaxed">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloremque quae, eaque nemo, dolorem voluptatum atque explicabo qui consequatur alias itaque ex molestias nam natus eum laboriosam voluptas at fugit!</p>
-                            <button
-                                class="cursor-pointer w-full flex items-center justify-center gap-2 rounded-lg h-10 bg-[#137fec] text-white text-sm font-bold tracking-[0.015em] hover:bg-[#137fec]/90 transition-colors shadow-md shadow-[#137fec]/10">
-                                <span class="material-symbols-outlined text-[18px]">how_to_vote</span>
-                                Cast Vote
-                            </button>
-                        </div>
-                    </div>
-                    <div
-                        class="bg-white rounded-xl border border-[#f0f2f4] overflow-hidden group hover:shadow-xl hover:shadow-[#137fec]/5 transition-all duration-300">
-                        <div class="relative h-48 overflow-hidden">
-                            <div class="h-full w-full bg-cover bg-center transition-transform duration-500 group-hover:scale-105"
-                                data-alt="Candidate Elena Rodriguez portrait">
-                                    <img src={image} alt="" />
-                                </div>
-                        </div>
-                        <div class="p-5">
-                            <h3 class="text-xl font-bold mb-1">test</h3>
-                            <p
-                                class="text-sm text-gray-500 line-clamp-6 mb-4 leading-relaxed">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloremque quae, eaque nemo, dolorem voluptatum atque explicabo qui consequatur alias itaque ex molestias nam natus eum laboriosam voluptas at fugit!</p>
-                            <button
-                                class="cursor-pointer w-full flex items-center justify-center gap-2 rounded-lg h-10 bg-[#137fec] text-white text-sm font-bold tracking-[0.015em] hover:bg-[#137fec]/90 transition-colors shadow-md shadow-[#137fec]/10">
-                                <span class="material-symbols-outlined text-[18px]">how_to_vote</span>
-                                Cast Vote
-                            </button>
-                        </div>
-                    </div>
-                    <div
-                        class="bg-white rounded-xl border border-[#f0f2f4] overflow-hidden group hover:shadow-xl hover:shadow-[#137fec]/5 transition-all duration-300">
-                        <div class="relative h-48 overflow-hidden">
-                            <div class="h-full w-full bg-cover bg-center transition-transform duration-500 group-hover:scale-105"
-                                data-alt="Candidate Elena Rodriguez portrait">
-                                    <img src={image} alt="" />
-                                </div>
-                        </div>
-                        <div class="p-5">
-                            <h3 class="text-xl font-bold mb-1">test</h3>
-                            <p
-                                class="text-sm text-gray-500 line-clamp-6 mb-4 leading-relaxed">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloremque quae, eaque nemo, dolorem voluptatum atque explicabo qui consequatur alias itaque ex molestias nam natus eum laboriosam voluptas at fugit!</p>
-                            <button
-                                class="cursor-pointer w-full flex items-center justify-center gap-2 rounded-lg h-10 bg-[#137fec] text-white text-sm font-bold tracking-[0.015em] hover:bg-[#137fec]/90 transition-colors shadow-md shadow-[#137fec]/10">
-                                <span class="material-symbols-outlined text-[18px]">how_to_vote</span>
-                                Cast Vote
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                    );
+                })}
+
             </section>
         </main>
         <div
@@ -98,7 +111,7 @@ export default function VoterBallot() {
             <div className="max-w-200 mx-auto flex items-center justify-between gap-4">
                 <div className="hidden md:block">
                     <p className="text-sm font-medium text-[#617589] ">Total Selections: <span
-                        className="text-[#137fec] font-bold">0/3</span></p>
+                        className="text-[#137fec] font-bold">{`0/${positions.length}`}</span></p>
                 </div>
                 <button
                     className="cursor-pointer w-full md:w-auto px-8 h-12 rounded-xl bg-[#137fec] text-white font-bold text-base hover:bg-[#137fec]/90 transition-all shadow-lg shadow-[#137fec]/25 flex items-center justify-center gap-2">
